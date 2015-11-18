524d35213a35cc3a039e194e498808a5c46e10ef
Tô Tướng Công <tcvn1985@gmail.com>
2015-11-18 23:19:00 +0700

    file cau hinh thu 2 cho COMPUTE NODE

--- LIBERTY-U14.04/com1-ipdd.sh
+++ LIBERTY-U14.04/com1-ipdd.sh
@@ -2,43 +2,7 @@
 
 source config.cfg
 
-ifaces=/etc/network/interfaces
-test -f $ifaces.orig || cp $ifaces $ifaces.orig
-rm $ifaces
-touch $ifaces
-cat << EOF >> $ifaces
-#Assign IP for Controller node
-
-# LOOPBACK NET 
-auto lo
-iface lo inet loopback
-
-# MGNT NETWORK
-auto eth0
-iface eth0 inet static
-address $COM1_MGNT_IP
-netmask $NETMASK_ADD_MGNT
-
-
-# EXT NETWORK
-auto eth1
-iface eth1 inet static
-address $COM1_EXT_IP
-netmask $NETMASK_ADD_EXT
-gateway $GATEWAY_IP_EXT
-dns-nameservers 8.8.8.8
-EOF
-
-
-echo "Configuring hostname in COMPUTE node"
-sleep 3
-echo "compute1" > /etc/hostname
-hostname -F /etc/hostname
-
-
-############################################
-
-#Update for Ubuntu
+echo "Update for Ubuntu"
 
 apt-get install software-properties-common -y
 add-apt-repository cloud-archive:liberty -y
@@ -49,7 +13,6 @@ echo "##### Configuring hostname for COMPUTE1 node... #####"
 sleep 3
 echo "compute1" > /etc/hostname
 hostname -F /etc/hostname
-
 
 iphost=/etc/hosts
 test -f $iphost.orig || cp $iphost $iphost.orig
@@ -89,10 +52,6 @@ gateway $GATEWAY_IP_EXT
 dns-nameservers 8.8.8.8
 
 EOF
-
-
-#
-
 
 sleep 5
 echo "##### Rebooting machine ... #####"
