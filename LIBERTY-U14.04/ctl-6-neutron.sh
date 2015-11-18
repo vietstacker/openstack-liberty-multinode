1178a56f6b2ef286a3ac972e0234d28b89b63398
Tô Tướng Công <tcvn1985@gmail.com>
2015-11-18 22:39:52 +0700

    fix loi dau $

--- LIBERTY-U14.04/ctl-6-neutron.sh
+++ LIBERTY-U14.04/ctl-6-neutron.sh
@@ -294,8 +294,8 @@ dns-nameservers 8.8.8.8
 
 auto eth1
 iface  eth1 inet manual
-up ip link set dev $IFACE up
-down ip link set dev $IFACE down
+up ip link set dev \$IFACE up
+down ip link set dev \$IFACE down
 
 EOF
 
